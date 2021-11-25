42d818ec8f1c3163b18d02a6d081ff3a07dda0c5
Anderson Fernandes <fernandesanderson14@gmail.com>
2021-11-25 18:57:00 -0300

    Ajustes de lint
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- src/Components/buttonSearchEntry.js
+++ src/Components/buttonSearchEntry.js
@@ -1,37 +1,47 @@
-import  React , { useState } from "react";
-import {Text, View, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
-import styles from "../Components/buttonStyles";
+import React, { useState } from 'react';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+} from 'react-native';
+import styles from '../Components/buttonStyles';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const ButtonSearchEntry = () => {
+  const [entryInfo, setEntryInfo] = useState(null);
 
-    const [entryInfo, setEntryInfo] = useState(null)
+  const [date, setDate] = useState('');
 
-    const [date, setDate] = useState('')    
+  const SearchEntry = async (key) => {
+    const value = await AsyncStorage.getItem(key);
+    setEntryInfo(value);
+  };
 
-    const SearchEntry = async (key) => {
-        const value = await AsyncStorage.getItem(key)
-        setEntryInfo(value)
-    }
-
-    return (
-        
-        <View style={styles.viewCheck}>
-            <View style={styles.buttonCheck}>
-                <Text>Entre com a Data</Text>
-                <TextInput 
-                    style={styles.input} 
-                    placeholder='ex. 01-01-2021'
-                    onChangeText={(key)=>setDate(key)}/>
-                <TouchableOpacity
-                    style={styles.buttonStyle}
-                    onPress={()=>{SearchEntry(date)}}>               
-                    <Text style={{textAlign:'center', marginTop: 15}}>BUSCAR HORÁRIO DE ENTRADA PARA</Text>                    
-                </TouchableOpacity> 
-                <Text style={styles.textCheck}>{entryInfo}</Text>
-            </View>                     
-        </View>
-    );
-}
+  return (
+    <View style={styles.viewCheck}>
+      <View style={styles.buttonCheck}>
+        <Text>Entre com a Data</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="ex. 01-01-2021"
+          onChangeText={(key) => setDate(key)}
+        />
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={() => {
+            SearchEntry(date);
+          }}
+        >
+          <Text style={{ textAlign: 'center', marginTop: 15 }}>
+            BUSCAR HORÁRIO DE ENTRADA PARA
+          </Text>
+        </TouchableOpacity>
+        <Text style={styles.textCheck}>{entryInfo}</Text>
+      </View>
+    </View>
+  );
+};
 
 export default ButtonSearchEntry;
--- src/pages/ShowScreen/index.js
+++ src/pages/ShowScreen/index.js
@@ -1,33 +1,34 @@
-import React from "react";
+import React from 'react';
 import { View, Text, SafeAreaView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { Button } from "react-native-elements/dist/buttons/Button";
+import { Button } from 'react-native-elements/dist/buttons/Button';
 
-import ButtonSearchEntry from "../../Components/buttonSearchEntry";
-import ButtonSearchExit from "../../Components/buttonSearchExit";
+import ButtonSearchEntry from '../../Components/buttonSearchEntry';
+import ButtonSearchExit from '../../Components/buttonSearchExit';
 
-import styles from "../../styles/styleShowScreen";
-import bs from "../../styles/button";
+import styles from '../../styles/styleShowScreen';
+import bs from '../../styles/button';
 
-export default function ShowScreen () {
-    const navigation = useNavigation();
-    navigation.setOptions({title: "Consultar Registro"});
-    
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.viewCheck}> 
-                <Text style={{fontWeight:'bold', margin: 10}}>HORA DA ENTRADA/SAIDA</Text>    
-                <ButtonSearchEntry></ButtonSearchEntry>
-                <ButtonSearchExit></ButtonSearchExit>           
-            </View>
-            <View style={{paddingBottom:100}} >
-                <Button
-                    buttonStyle={bs.buttonBack2}
-                    title= "VOLTAR "
-                    onPress={()=>{navigation.navigate('LoginScreen')}}
-                
-                />
-            </View>
-        </SafeAreaView>    
-    );
+export default function ShowScreen() {
+  const navigation = useNavigation();
+  navigation.setOptions({ title: 'Consultar Registro' });
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.viewCheck}>
+        <Text style={{ fontWeight: 'bold', margin: 10 }}>
+          HORA DA ENTRADA/SAIDA
+        </Text>
+        <ButtonSearchEntry />
+        <ButtonSearchExit />
+      </View>
+      <View style={{ paddingBottom: 100 }}>
+        <Button
+          buttonStyle={bs.buttonBack2}
+          title="VOLTAR "
+          onPress={() => navigation.navigate('LoginScreen')}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
