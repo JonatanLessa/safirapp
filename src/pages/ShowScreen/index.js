e63a1a7d34cdbca0ba9c09b360d144fd1819c6a2
JonatanLessa <jls15@aluno.ifal.edu.br>
2022-01-09 01:26:06 -0300

    Exibe dados registrados para a data selecionada

--- src/pages/ShowScreen/index.js
+++ src/pages/ShowScreen/index.js
@@ -7,72 +7,75 @@ import {
   } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Button } from 'react-native-elements/dist/buttons/Button';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import database from '../../config/firebaseconfig';
+import { getDatabase, ref, onValue, connectDatabaseEmulator } from 'firebase/database';
+
+import app from '../../config/firebaseconfig';
 
 import styles from '../../styles/styleShowScreen';
 import bs from '../../styles/button';
 import ButtonDataPicker from '../../Components/ButtonDataPicker';
 import ButtonRegisterSearch from '../../Components/ButtonRegisterSeach';
 
+
 export default function ShowScreen() {
+
   const navigation = useNavigation();
-  const [users, setUsers] = useState([]);
+
+  const [data, setData] = useState([]);
+
+  const [date, setDate] = useState('');
+
+  const getDataFromService = () => {
+    const db = getDatabase();
+    const reference = ref(db, 'registroPonto');
+
+    onValue(reference, (snapshot) => {
+
+    const datas = [];
+    snapshot.forEach(data => {
+      datas.push({
+        id: data.key,
+        data: data.val().date,
+        hora: data.val().hour  
+      });
+    });
+    
+    setData(datas);  
+    imprime();       
+    })
+  }
+  const imprime = () => {
+    data.forEach(function(nome) {      
+      if (nome.data == date) {
+        console.log(nome.data)
+        console.log(nome.hora)
+      }
+    });
+  }
   
   useEffect (()=> {
     navigation.setOptions({ title: 'CONSULTAR PONTO' });
   },[]);
-
-  const [entryInfo, setEntryInfo] = useState(null);
-  const [exitInfo, setExitInfo] = useState(null);
-
-  const [date, setDate] = useState('');
-
-  const SearchEntry = async (key) => {
-    const value = await AsyncStorage.getItem(key);
-    setEntryInfo(value);
-  };
-
-  const SearchExit = async (key) => {
-    const value = await AsyncStorage.getItem(key);
-    setExitInfo(value);
-  };
-
-  function getInfoEntry( id ) {    
-    setUsers(database.collection("users").doc(`ENTRY-${id}`).get())
- };
-
+ 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.viewCheck}>     
-          
+      <View style={styles.viewCheck}>               
         <Text style={{ fontWeight: 'bold', margin: 10 }}>
           HORA DA ENTRADA/SAIDA
-        </Text> 
-
-        <ButtonRegisterSearch
-          title={"HORÁRIO FINAL NOVO"}
-          value={users}
-          onPress={() => {            
-            getInfoEntry("09-12-2021")}                
-          } 
-              
-        /> 
+        </Text>        
         <ButtonDataPicker
           title={"SELECIONAR DATA DE ENTRADA"}
-          value={entryInfo}
+          value={date}
           onDateSelect={(selectedDate) => {
             setDate(selectedDate)
-            SearchEntry(`ENTRY-${date}`);
           }}
-        />                
-        <ButtonDataPicker
-          title={"SELECIONAR DATA DE SAÍDA"}
-          value={exitInfo}
-          onDateSelect={(selectedDate) => {
-            setDate(selectedDate)
-            SearchExit(`EXIT-${date}`);
-          }}
+        />        
+         <ButtonRegisterSearch
+          title={"LISTAR"}
+          value={''}
+          onPress={() => {            
+            getDataFromService()             
+          }}               
         />
       </View>
       <View style={{ paddingBottom: 100 }}>
@@ -99,26 +102,3 @@ const styles2 = StyleSheet.create({
     width: 280,
   },
 });
-/* 
-        <Text>Entre com a Data</Text>
-          <TextInput
-            style={styles2.input}
-            placeholder="ex. 01-01-2021"
-            onChangeText={(key) => setDate(key)}
-          />       
-        <ButtonRegisterSearch
-          title={"BUSCAR HORÁRIO DE SAÍDA PARA"}
-          value={exitInfo}
-          onPress={() => {
-            SearchExit(`EXIT-${date}`);
-          }}
-        />
-
-
-        {users.map((user) => {
-          return (
-            <Text>Nome:{user.data}</Text>
-         )
-        })} 
-         
-          */
