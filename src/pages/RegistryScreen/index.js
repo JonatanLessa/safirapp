4d8a27c1c95d35655651339a10b848f273bc608d
JonatanLessa <jls15@aluno.ifal.edu.br>
2022-01-09 01:09:27 -0300

    Registra data e hora no Realtime Database

--- src/pages/RegistryScreen/index.js
+++ src/pages/RegistryScreen/index.js
@@ -3,102 +3,74 @@ import { Text, View, SafeAreaView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useNavigation } from '@react-navigation/native';
 import { Button } from 'react-native-elements/dist/buttons/Button';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import moment from 'moment';
 
-import database from '../../config/firebaseconfig';
+import app from '../../config/firebaseconfig';
+import { getDatabase, ref, set, push } from 'firebase/database';
 
 import styles from '../../styles/style';
 import bs from '../../styles/button';
 
-import ButtonRegisterSearch from '../../Components/ButtonRegisterSeach';
+import ModalConfirmation from '../../Components/ModalConfirmation';
 
 export default function RegistryScreen() {
   const navigation = useNavigation();
-  const [id, setId] = useState('');
   
   useEffect (()=> {
     navigation.setOptions({ title: 'REGISTRAR PONTO' });
-  },[]);
-
-  useEffect (()=> {
-    navigation.setOptions({ title: 'REGISTRAR PONTO' });
-  },[id]);
-
-
+  },[]);  
 
   const greetings = () => {
     return `Olá ${'Marcelo'}!`;
   };
  
-  const [entryTime, setEntryTime] = useState(0); 
-  const [exitTime, setExitTime] = useState(0); 
-  
-  const StoreEntry = (key, value) => {
-    AsyncStorage.setItem(key, value);
-  };
+  const [date, setDate] = useState(''); 
+  const [hour, setHour] = useState('');
+  const [visibleModal, setVisibleModal] = useState(false);
 
-  const StoreExit = (key, value) => {
-    AsyncStorage.setItem(key, value);
-  };
+  const addData = () => {
+      setDate(moment().format('DD-MM-YYYY').toString());
+      setHour(moment().format('hh:mm:ss a').toString());           
+  }
 
-  function createInfoEntry( newHour ) {    
-     database.collection("users").doc(`ENTRY-${id}`).set({[id]: newHour
-      //description, newHour
-    })
-  };
-
-  function createInfoExit( newHour ) {    
-    database.collection("users").doc(`EXIT-${id}`).set({[id]: newHour
-     //description, newHour
-   })
- };
+  const addDate = () => {
+    const db = getDatabase();
+    const reference = ref(db, 'registroPonto');
+    push(reference, {
+      "date" : date,
+      "hour" : hour
+    });
+  }
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
         <Text>{greetings()}</Text>
-        <Text>Registre aqui os seus horários:</Text>
-        <ButtonRegisterSearch
-          title={"HORÁRIO INICIAL NOVO"}
-          value={entryTime}
-          onPress={() => {
-            setId(moment().format('DD-MM-YYYY').toString(), 
-            createInfoEntry(moment().format('hh:mm:ss a').toString()))}                
-          }      
-        /> 
-        <ButtonRegisterSearch
-          title={"HORÁRIO FINAL NOVO"}
-          value={exitTime}
-          onPress={() => {
-            setId(moment().format('DD-MM-YYYY').toString(), 
-            createInfoExit(moment().format('hh:mm:ss a').toString()))}                
-          }      
-        /> 
-       <ButtonRegisterSearch
-          title={"HORÁRIO INICIAL"}
-          value={entryTime}
-          onPress={() => {setEntryTime(moment().format('DD-MM-YYYY hh:mm:ss a'));
-            {
-              StoreEntry(
-                `ENTRY-${moment().format('DD-MM-YYYY').toString()}`,
-                moment().format('hh:mm:ss a').toString(),
-              );
-            }                
-          }}            
-        />                 
-        <ButtonRegisterSearch
-          title={"HORÁRIO FINAL"}
-          value={exitTime}
-          onPress={() => {setExitTime(moment().format('DD-MM-YYYY hh:mm:ss a'));
-            {
-              StoreExit(
-                `EXIT-${moment().format('DD-MM-YYYY').toString()}`,
-                moment().format('hh:mm:ss a').toString(),
-              );
-            }                
-          }}           
-        />
+        <Text>Registre aqui os seus horários:</Text>        
+        <ModalConfirmation
+            title={"REGISTRAR ENTRADA"}
+            valueDate={date}
+            valueHour={hour}
+            visible={visibleModal}
+            setVisible={setVisibleModal}
+            onConfirm={(addDate)}
+            onPress={()=>{
+              setVisibleModal(true)
+              addData()
+            }}
+        />  
+        <ModalConfirmation
+            title={"REGISTRAR SAÍDA"}
+            valueDate={date}
+            valueHour={hour}
+            visible={visibleModal}
+            setVisible={setVisibleModal}
+            onConfirm={(addDate)}
+            onPress={()=>{
+              setVisibleModal(true)
+              addData()
+            }}
+        />    
       </View>
       <Button
         buttonStyle={bs.buttonBack}
