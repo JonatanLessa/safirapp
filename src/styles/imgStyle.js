cf19470713979c55eaeda1b355dd2baaee0864f0
JonatanLessa <jls15@aluno.ifal.edu.br>
2022-01-12 08:50:52 -0300

    Adiciona stylo as imagens da tela de dispositivos

--- src/styles/imgStyle.js
+++ src/styles/imgStyle.js
@@ -5,4 +5,15 @@ export default StyleSheet.create({
     width: 250,
     resizeMode: 'contain',
   },
+  waterPump: {
+    //borderTopRightRadius: 20,
+    //borderBottomRightRadius: 20,
+    //borderBottomLeftRadius: 20,
+    //borderTopLeftRadius: 20,
+    width: 100,
+    height: 100,
+    //resizeMode: 'contain',
+    borderWidth: 2,
+    borderColor: "gray"
+  },
 });
