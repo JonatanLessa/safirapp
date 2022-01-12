3b78ca24f19f202f068389b0127d5e431e63c590
JonatanLessa <jls15@aluno.ifal.edu.br>
2022-01-12 08:53:56 -0300

    Inlcue componente a conter todos stylos para views

---
+++ src/styles/viewStyle.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    viewDeviceActivationScreen: {
+      display: "flex",
+      flexDirection: "vertical",
+      justifyContent: "space-around",
+      alignItems: "center",
+      height: "100%",
+      textAlign: "center"
+    }
+  });
+
+export default styles;
