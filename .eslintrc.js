2037b3c0982d3aa7423be9014e5964d28a83f6de
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-01-13 21:44:55 -0300

    Adicionando eslint
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,24 @@
-// .eslintrc.js
-
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "react-native/react-native": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "react-native"
+    ],
+    "rules": {
+    }
 };
