82daa4d4f5e8ae126f6e07fdbb1c8c996dac4dbe
Marcelo Melo <marcelomelo.berto@hotmail.com>
2022-01-16 01:17:14 -0300

    Exclusão da tela LoginScreen (Redundância de funcionalidades)

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 
-import LoginScreen from './src/pages/LoginScreen';
 import RegistryScreen from './src/pages/RegistryScreen';
 import ShowScreen from './src/pages/ShowScreen';
 import DeviceActivationScreen from './src/pages/DeviceActivationScreen';
@@ -18,30 +17,25 @@ export default function App() {
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
 
-            if (route.name === 'Home') {
-              iconName = focused ? 'home-outline' : 'home';
-            } 
-            else if (route.name === 'Registry') {
+            if (route.name === 'Registro') {
               iconName = focused ? 'finger-print-outline' : 'finger-print';
             }
-            else if (route.name === 'Report') {
+            else if (route.name === 'Relatório') {
               iconName = focused ? 'reader-outline' : 'reader';
             }
-            else if (route.name === 'Devices') {
+            else if (route.name === 'Dispositivos') {
               iconName = focused ? 'flash-outline' : 'flash';
             }
 
-            // You can return any component that you like here!
             return <Ionicons name={iconName} size={size} color={color} />;
           },
           tabBarActiveTintColor: '#8CDB94',
           tabBarInactiveTintColor: '#000',
         })}
       >
-        <Tab.Screen name="Home" component={LoginScreen} />
-        <Tab.Screen name="Registry" component={RegistryScreen} />
-        <Tab.Screen name="Report" component={ShowScreen} />
-        <Tab.Screen name="Devices" component={DeviceActivationScreen} />
+        <Tab.Screen name="Registro" component={RegistryScreen} />
+        <Tab.Screen name="Relatório" component={ShowScreen} />
+        <Tab.Screen name="Dispositivos" component={DeviceActivationScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
