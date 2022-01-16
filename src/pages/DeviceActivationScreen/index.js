3db1f2fede84d6f0d224541d9d7ac16ebae0a425
JonatanLessa <jls15@aluno.ifal.edu.br>
2022-01-16 00:46:01 -0300

    Configura a integração de controle de dispositivos

--- src/pages/DeviceActivationScreen/index.js
+++ src/pages/DeviceActivationScreen/index.js
@@ -5,10 +5,14 @@ import {
   Image,
   SafeAreaView, 
   } from 'react-native';
+//import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
 import { Button } from 'react-native-elements/dist/buttons/Button';
 
 import app from '../../config/firebaseconfig';
+import { getDatabase, ref, set, push } from 'firebase/database';
+//import init from 'react_native_mqtt';
+//import uuid from 'react-native-uuid';
 
 import styles from '../../styles/styleShowScreen';
 import bs from '../../styles/button';
@@ -18,12 +22,35 @@ import imgStyle from '../../styles/imgStyle';
 
 
 export default function DeviceActivationScreen() {
-  const navigation = useNavigation();
+    const navigation = useNavigation();
   
-  useEffect (()=> {
-    navigation.setOptions({ title: 'ATIVAÇÃO DE DISPOSITIVOS' });
-  },[]);
+    useEffect (()=> {
+        navigation.setOptions({ title: 'ATIVAÇÃO DE DISPOSITIVOS' });
+    },[]);
+
+    useEffect (()=> {
+	    setState(0);
+	},[])
+
+    const [state, setState] = useState();
+
+    const estate = () => {
+        if( state == 0 ) {
+            setState(1);
+        } else {
+            setState(0);
+        }
+    }
   
+    const addStateDevice = (type) => {
+    console.log(type);
+    console.log(state);
+    const db = getDatabase();
+    const reference = ref(db, type);
+        set(reference, {
+            "estado" : state
+        });
+    }
     
   return (
     <SafeAreaView style={styles.container}>
@@ -40,8 +67,11 @@ export default function DeviceActivationScreen() {
                     <Button
                         buttonStyle={bs.buttonBack}
                         title="ATIVAR "
-                        onPress={() => navigation.navigate('LoginScreen')}
-                    />
+                        onPress={() => {
+                            estate();
+                            addStateDevice("bombaDAgua")
+                        }}
+                    />                
                 </View>
             </View> 
             <View style={{ alignItems: "flex-start", flexDirection: "column",  }}>
@@ -53,7 +83,10 @@ export default function DeviceActivationScreen() {
                     <Button
                         buttonStyle={bs.buttonBack}
                         title="ATIVAR "
-                        onPress={() => navigation.navigate('LoginScreen')}
+                        onPress={() => {
+                            setState(true)
+                            addStateDevice("registro01")
+                        }}
                     />
                 </View>
             </View> 
@@ -66,7 +99,10 @@ export default function DeviceActivationScreen() {
                     <Button
                         buttonStyle={bs.buttonBack}
                         title="ATIVAR "
-                        onPress={() => navigation.navigate('LoginScreen')}
+                        onPress={() => {
+                            setState(true)
+                            addStateDevice("registro02")
+                        }}
                     />
                 </View>
             </View> 
@@ -84,43 +120,6 @@ export default function DeviceActivationScreen() {
   );
 }
 /*
-<View>
-            <Text style={{  fontWeight: 'bold', marginTop: 20,}}> ESCOLHA O DISPOSITIVO: </Text>                
-        </View>
-            <View style={{ alignItems: "flex-start", flexDirection: "column", justifyContent: "space-between",}}>
-                <View>  
-                    <Text> REGISTRO SETOR 01: </Text> 
-                </View>           
-                <View style={{ alignItems: "center", flexDirection: "row", marginRight: 50, marginTop: 100 }}>                 
-                    <Image source={sprinkler} style={imgStyle.waterPump}/> 
-                    <Button
-                        buttonStyle={bs.buttonBack}
-                        title="ATIVAR "
-                        onPress={() => navigation.navigate('LoginScreen')}
-                    /> 
-                </View>
-            </View>
-            <View style={{ alignItems: "flex-start", flexDirection: "column", justifyContent: "space-between",}}>
-                <View>
-                    <Text> REGISTRO SETOR 02: </Text>   
-                </View>               
-                <View style={{ alignItems: "center", flexDirection: "row", marginRight: 50, marginTop: 100}}>
-                    <Image source={sprinkler} style={imgStyle.waterPump}/>
-                    <Button
-                        buttonStyle={bs.buttonBack}
-                        title="ATIVAR "
-                        onPress={() => navigation.navigate('LoginScreen')}
-                    /> 
-                </View>                       
-            </View>            
 
-
-
-<View>
-      <Image source={sprinkler} style={imgStyle.logo}/>
-      </View>
-      <Text> REGISTRO SETOR 01: </Text>
-      <View>
-      <Image source={sprinkler} style={imgStyle.logo}/>
-      </View>
-      <Text> REGISTRO SETOR 02: </Text>*/
+    
+*/
