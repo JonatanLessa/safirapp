853f0a0fb585e4b122a1cd94994fde0c78c8de1a
JonatanLessa <jls15@aluno.ifal.edu.br>
2022-01-19 17:34:05 -0300

    Cria style para RegistryScreen

---
+++ src/styles/styleRegistryScreen.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  viewModal: {
+    marginTop: 20,
+    marginVertical: -20,
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  }, 
+});
+
+export default styles;
